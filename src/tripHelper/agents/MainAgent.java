1f9ae6cb997bac6ecc132b5d573b4a6cf347afad
hubertwojciechowski <hubertwojciechowski@hotmail.com>
2012-06-03 18:20:08 +0200

    Sending simple configuration to PathAgents

--- src/tripHelper/agents/MainAgent.java
+++ src/tripHelper/agents/MainAgent.java
@@ -1,19 +1,25 @@
 package tripHelper.agents;
-
+import java.io.IOException;
 import java.util.ArrayList;
-
+import tripHelper.utils.PathAgentConfiguration;
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+
+
+
 
 
 
 public class MainAgent extends Agent {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = -3194090991124107894L;
 	private ArrayList<AID> calculationAgents;
+	
+	public static final boolean DEBUG = true;
+	
 	
 	
 	public MainAgent () {
@@ -24,10 +30,57 @@ public class MainAgent extends Agent {
 		calculationAgents.add(new AID("pa2", AID.ISLOCALNAME ));
 		
 	}
-	
-	
+
 	protected void setup() {
 
+		// Wysylanie konfiguracji do agentow obliczeniowych
+		this.addBehaviour(this.configureCalculationAgents());
+		
+		
+	}
+	
+	
+	// Behaviours
+	
+	private OneShotBehaviour configureCalculationAgents() {
+		
+		return new OneShotBehaviour() {
+			
+			private static final long serialVersionUID = -6435156850120548042L;
+
+			@Override
+			public void action() {
+				
+				int precission = 1;
+				
+				// Creating message with configuration for each agent 
+				for (AID agent : calculationAgents) {
+					
+					// Configuration
+					PathAgentConfiguration agentConfiguration = new PathAgentConfiguration(precission);
+					ACLMessage configurationMsg = new ACLMessage(ACLMessage.INFORM);
+					
+					// Serialize configuration
+					configurationMsg.addReceiver(agent);
+					try {
+						configurationMsg.setContentObject(agentConfiguration);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					
+					if (DEBUG) {
+						System.out.println(String.format("%s : Wysylam precyje (%d) do %s ", myAgent.getLocalName(), precission , agent.getLocalName()  ));
+					}
+					
+					// Precisson incrementation
+					precission++;
+					
+					// Sending
+					myAgent.send(configurationMsg);
+				}
+				
+			}
+		};
 	}
 	
 	
---
+++ src/tripHelper/utils/PathAgentConfiguration.java
@@ -0,0 +1,14 @@
+package tripHelper.utils;
+
+import jade.util.leap.Serializable;
+
+public class PathAgentConfiguration implements Serializable {
+	
+	
+	public int precission;
+	
+	public PathAgentConfiguration(int precission) {
+		this.precission = precission;
+	}
+	
+}
