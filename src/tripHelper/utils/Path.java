10898059c30a52fa4e9690cbe3512193cec642a4
hubertwojciechowski <hubertwojciechowski@hotmail.com>
2012-06-03 17:31:59 +0200

    Add Jade Libs to repo

--- src/tripHelper/utils/Path.java
+++ src/tripHelper/utils/Path.java
@@ -1,5 +1,16 @@
 package tripHelper.utils;
 
+import java.util.ArrayList;
+
 public class Path {
 
+	/*
+	 * Kolejne kroki sciezki
+	 */
+	public ArrayList<Segment> steps;
+	
+	
+	
+	
+	
 }
--- src/tripHelper/utils/Segment.java
+++ src/tripHelper/utils/Segment.java
@@ -1,11 +1,12 @@
 package tripHelper.utils;
 
 public class Segment {
-
 	
 	public String from;
 	public String to;
 	public float cost;
+	public type trasportType;
 	
-	
+	public static enum type {none,train,bus,plain}; 
+
 }
